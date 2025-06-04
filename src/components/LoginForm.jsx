bec2f212bb926550566e08b865da788e83a24fac
user.name : aaradhayasingh811 <user.email : aaradhayasingh811@gmail.com>
2025-06-04 14:33:45 +0530

    uncommented rate limiting

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -58,9 +58,9 @@ const LoginForm = () => {
       newErrors.password = "Password is required";
     }
 
-    if (!captchaVerified) {
-      newErrors.captcha = "Please complete the CAPTCHA";
-    }
+    // if (!captchaVerified) {
+    //   newErrors.captcha = "Please complete the CAPTCHA";
+    // }
 
     setErrors(newErrors);
     return Object.keys(newErrors).length === 0;
@@ -275,7 +275,7 @@ const LoginForm = () => {
               </div>
             </div>
 
-            <div className="mt-4">
+            {/* <div className="mt-4">
               <div className="flex justify-center">
                 <ReCAPTCHA
                   sitekey={RECAPTCHA_SITE_KEY}
@@ -288,7 +288,7 @@ const LoginForm = () => {
                   {errors.captcha}
                 </p>
               )}
-            </div>
+            </div> */}
 
             <button
               type="submit"
